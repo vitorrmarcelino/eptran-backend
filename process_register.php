1290bf9a1b8e98712a8cdf936f0404728902816a
Vitor Rafael Marcelino <111241590+vitorrmarcelino@users.noreply.github.com>
2024-10-02 11:42:04 -0300

    Add files via upload

--- process_register.php
+++ process_register.php
@@ -3,7 +3,21 @@ include "dbconnect.php";
 
 session_start();
 
-$query = "INSERT INTO usuarios ();";
+$nome = $_POST['nome'];
+$senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
+$email = $_POST['email'];
+$genero = $_POST['genero'];
+$cpf = $_POST['cpf'];
+$nascimento = $_POST['nascimento'];
+$escolaridade = $_POST['escolaridade'];
+$cep = $_POST['cep'];
+$bairro = $_POST['bairro'];
+$municipio = $_POST['municipio'];
+$escola = $_POST['escola'];
+$uf = $_POST['uf'];
+
+$query = "INSERT INTO usuarios (nome, senha, adm, email, genero, cpf, nascimento, escolaridade, cep, bairro, municipio, escola, uf) 
+VALUES ('$nome', '$senha', false, '$email', '$genero', '$cpf', '$nascimento', '$escolaridade', '$cep', '$bairro', '$municipio', '$escola', '$uf')";
 
 $executa_insert = mysqli_query($conn, $query);
 
@@ -20,5 +34,4 @@ if($executa_insert){
     window.location.href = '../cadastro1.php';
     </script>";
 }
-
 ?>
